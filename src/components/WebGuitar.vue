02325967f2f18d710f1bf460e48a29c1acf23d10
Nick Chite <nicholas.chite@gmail.com>
2026-06-21 14:31:40 -0400

    web guitar loading issue

--- src/components/WebGuitar.vue
+++ src/components/WebGuitar.vue
@@ -26,6 +26,10 @@ async function loadGuitarScript() {
   await injectScript('https://d3js.org/d3.v6.min.js', 'data-d3-script');
   await injectScript('https://cdn.jsdelivr.net/npm/tone@14.8.26', 'data-tone-script');
   await injectScript('/resources/guitar/src/script.js', GUITAR_SCRIPT_ATTR);
+
+  if (typeof window.initWebGuitar === 'function') {
+    window.initWebGuitar();
+  }
 }
 
 onMounted(() => {
